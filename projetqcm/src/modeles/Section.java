90f763a1b126d0742cbc0518bcfe820d8121fead
steve.lefort <steve.lefort@991af220-a019-11dd-a887-7ffe1a420f8d>
2008-10-22 13:31:42 +0000

    Classe Section du projet QCM Accesseur par defaut constructeur

---
+++ projetqcm/src/modeles/Section.java
@@ -0,0 +1,93 @@
+package modeles;
+
+import java.util.UUID;
+import java.util.Vector;
+
+public class Section {
+	/*****************************************************
+	*						Attributs					 *
+	******************************************************/
+
+	private int numero;
+	private String nom;
+	private int nbrQuestion;
+	private Vector<Question> questions;
+	
+	
+	/*****************************************************
+	*						Constructeurs				 *
+	******************************************************/
+	
+	public Section()
+	{
+		super();
+		this.setNumero(0);
+		this.setNom("Nouvelle Section");
+		this.setNbrQuestion(1);
+		this.setQuestions(new Vector<Question>());
+	}
+	
+	public Section(String nom)
+	{
+		this();
+		this.setNom(nom);
+	}
+	
+	public Section(String nom,int num)
+	{
+		this(nom);
+		this.setNumero(num);
+	}
+
+	public Section(String nom,int num, int nbrQuestion)
+	{
+		this(nom,num);
+		this.setNbrQuestion(nbrQuestion);
+	}
+
+	
+		
+	/*****************************************************
+	*						Accesseur					 *
+	******************************************************/
+	public int getNumero() {
+		return numero;
+	}
+
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+
+	public String getNom() {
+		return nom;
+	}
+
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+
+	public int getNbrQuestion() {
+		return nbrQuestion;
+	}
+
+
+	public void setNbrQuestion(int nbrQuestion) {
+		this.nbrQuestion = nbrQuestion;
+	}
+
+
+	public Vector<Question> getQuestions() {
+		return questions;
+	}
+
+
+	public void setQuestions(Vector<Question> questions) {
+		this.questions = questions;
+	}
+	
+	
+}
